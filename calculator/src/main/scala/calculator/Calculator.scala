7c99591ae06c3928380a02de1339b70a910c03b5
Jichao Ouyang <oyanglulu@gmail.com>
2016-07-22 22:54:49 +0800

    calculator

--- calculator/src/main/scala/calculator/Calculator.scala
+++ calculator/src/main/scala/calculator/Calculator.scala
@@ -11,13 +11,27 @@ final case class Divide(a: Expr, b: Expr) extends Expr
 object Calculator {
   def computeValues(
       namedExpressions: Map[String, Signal[Expr]]): Map[String, Signal[Double]] = {
-    ???
+    namedExpressions.map {
+      case (name, s) => name -> Signal {
+        eval(s(), namedExpressions)
+      }
+    }
   }
 
   def eval(expr: Expr, references: Map[String, Signal[Expr]]): Double = {
-    ???
+    def evalRefWithPath(expr: Expr, path: List[Expr]): Double = expr match {
+      case Ref(name) if path.contains(expr) => Double.NaN
+      case Ref(name) => evalRefWithPath(getReferenceExpr(name, references), expr::path)
+      case Literal(v) => v
+      case Plus(a, b) => evalRefWithPath(a, path) + evalRefWithPath(b, path)
+      case Minus(a, b) => evalRefWithPath(a, path) - evalRefWithPath(b, path)
+      case Times(a, b) => evalRefWithPath(a, path) * evalRefWithPath(b, path)
+      case Divide(a, b) => evalRefWithPath(a, path) / evalRefWithPath(b, path)
+    }
+    evalRefWithPath(expr, Nil)
   }
 
+  
   /** Get the Expr for a referenced variables.
    *  If the variable is not known, returns a literal NaN.
    */
